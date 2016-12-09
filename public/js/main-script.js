47cc0071575ea14f3e7c2f47d4804aca43915289
Trairat Punyawatthananukool <samoooop@gmail.com>
2016-12-09 22:49:54 +0700

    add getScene()

--- public/js/main-script.js
+++ public/js/main-script.js
@@ -6,13 +6,14 @@ var height = 30;
 var viewportWidth = GLDom.offsetWidth;
 var viewportHeight = GLDom.offsetHeight;
 var aspect = width / height;
-var camera = new THREE.OrthographicCamera(-1,1,1,-1,-10,1000);
+var camera = new THREE.OrthographicCamera(-1,1,-1,1,-10,10);
 var renderer = new THREE.WebGLRenderer();
 var dbCanvasDom = document.getElementById("debugCanvas");
 var dbCanvasCtx = dbCanvasDom.getContext("2d");
 var pixels = new Uint8Array(width * height * 4);
 var float_pixels = new Float32Array(width * height * 4);
 var gl;
+var cubes = new Uint8Array(10 * 10 * 4);
 renderer.setSize( viewportWidth, viewportHeight );
 dbCanvasDom.width = width;
 dbCanvasDom.height = height;
@@ -77,13 +78,17 @@ function render(frameTime, time) {
     // camera.position.y = cameraDefault.position.y + Math.sin(time / 1000);
     // camera.position.x = cameraDefault.position.y + Math.cos(time / 1000);
     //plane.rotation.x += 0.01;
+    
+    for(var i =0; i<cubes.length;i++)
+        cubes[i]=1;
+    var s = getScene(cubes);
     renderer.render( bufferScene, camera, bufferTexture );
-    renderer.render( scene, camera );
+    renderer.render( s, camera );
     // renderer.render( scene, camera );
     renderer.readRenderTargetPixels(bufferTexture,0 ,0,width,height,float_pixels);
     //var canvas = document.querySelector('canvas');
     //renderer.readRenderTargetDepthPixels(canvas.getContext('webgl2'),bufferTexture,0 ,0,width,height,float_pixels);
-    console.log(float_pixels);
+    //console.log(float_pixels);
     //console.log(pixels.filter(function(x){return x!=0 && x!=255}));
     /// Canvas Code
     var counter = 0;
@@ -104,34 +109,24 @@ function render(frameTime, time) {
 
 }
 
-readRenderTargetPixels = function ( renderTarget, x, y, width, height, buffer ) {
+function getScene(cubes) {
+    var scene = new THREE.Scene();
+    var di = Math.sqrt(cubes.length/4);
 
-    		var framebuffer = properties.get( renderTarget ).__webglFramebuffer;
-
-    		if ( framebuffer ) {
-
-    			var restore = false;
-
-    			if ( framebuffer !== _currentFramebuffer ) {
-
-    				_gl.bindFramebuffer( _gl.FRAMEBUFFER, framebuffer );
-
-    				restore = true;
-
-    			}
-
-    			try {
-    					_gl.readPixels( x, y, width, height, gl.RGB, gl.FLOAT, buffer );
-    			} finally {
-
-    				if ( restore ) {
-
-    					_gl.bindFramebuffer( _gl.FRAMEBUFFER, _currentFramebuffer );
-
-    				}
-
-    			}
-
-    		}
-
-    	};
+    var geometry = new THREE.BoxGeometry( 2/di, 2/di, 2/di );
+    var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+    for(var index=0; index<cubes.length; index+=4){
+        if(cubes[index+1] != 0){
+            var color = Math.floor(Math.random()*16777215);
+            var material = new THREE.MeshBasicMaterial( {color: color} );
+            var cube = new THREE.Mesh( geometry, material );
+            var corX = (index / 4) % di;
+            var corY = Math.floor((index / (4 * di)));
+            console.log(corX + " " + corY);
+            cube.position.x = corX*2/di-1+1/di;
+            cube.position.y = corY*2/di-1+1/di;
+            scene.add(cube);
+        }
+    }
+    return scene;
+}
